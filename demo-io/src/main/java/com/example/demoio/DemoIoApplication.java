5e2a1c95d4431e295f7afdbb416fcbbc2b93eca5
xiaolei.he <xiaolei.he@qunar.com>
2022-07-17 15:35:42 +0800

    添加

--- demo-io/src/main/java/com/example/demoio/DemoIoApplication.java
+++ demo-io/src/main/java/com/example/demoio/DemoIoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demoio;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @SpringBootApplication
 public class DemoIoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoIoApplication.class, args);
-	}
+    /**
+     * 实体
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //SpringApplication.run(DemoIoApplication.class, args);
+        List<String> list = null;
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
 
 }
--- demo-io/src/test/java/com/example/demoio/DemoIoApplicationTests.java
+++ demo-io/src/test/java/com/example/demoio/DemoIoApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demoio;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoIoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
