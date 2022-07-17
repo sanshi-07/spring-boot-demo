32e63d93cfd6659948fc112368ef3205fb893c0a
xiaolei.he <xiaolei.he@qunar.com>
2022-07-17 15:34:58 +0800

    io

---
+++ demo-io/src/main/java/com/example/demoio/DemoIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIoApplication.class, args);
+	}
+
+}
